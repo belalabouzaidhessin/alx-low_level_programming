99b840516a31c167b4bf54e4c5a9c7546a3514af
belalabouzaidhessin <belalabouzaid55@gmail.com>
2023-09-07 19:47:03 -0400

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    // Use the puts function to print the message
+    
     puts("Programming is like building a multilingual puzzle");
 
-    // End the program with the value 0
     return 0;
 }
