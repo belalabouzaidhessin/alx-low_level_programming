296f486a5d18b34cff3cab694901580556ff3341
belalabouzaidhessin <belalabouzaid55@gmail.com>
2023-09-12 18:17:22 -0400

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,19 @@
-#include <stdio.h>
-int main () {
+#include "main.h"
 
-printf("_putchar\n");
-
-return 0;
-
-
-}	
+/**
+ * main - Print _putchar
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
