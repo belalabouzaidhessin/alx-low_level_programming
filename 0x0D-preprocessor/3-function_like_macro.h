8b4569d88dba679cfc91ce34e1ecc5ee99c92f70
belalabouzaidhessin <belalabouzaid55@gmail.com>
2023-10-10 19:41:59 -0400

    Function-like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,13 +1,6 @@
-#include <stdio.h>
+#ifndef BELAL_ABOUZAID_H
+#define BELAL_ABOUZAID_H
 
 #define ABS(x) ((x) < 0 ? -(x) : (x))
 
-int main() {
-    int num = -10;
-    int result = ABS(num);
-
-    printf("Absolute value of %d is %d\n", num, result);
-
-    return 0;
-}
-
+#endif
