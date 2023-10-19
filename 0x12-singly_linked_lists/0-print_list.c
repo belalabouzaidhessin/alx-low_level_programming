83036d4aea16104caaf364824148e8d83cfbbbce
belalabouzaidhessin <belalabouzaid55@gmail.com>
2023-10-19 14:32:53 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	int x = 0;
+	int count = 0;
 
 	while (h)
 	{
@@ -22,9 +22,9 @@ size_t print_list(const list_t *h)
 		{
 		printf("[%d] %s\n", h->len, h->str);
 		}
-		x++;
+		count++;
 		h = h->next;
 	}
-	return (x);
+	return (count);
 }
 
