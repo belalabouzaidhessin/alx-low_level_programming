bb39c858ff8713f6ec565b198d23635ad0398f63
belalabouzaidhessin <belalabouzaid55@gmail.com>
2023-10-24 15:00:49 -0700

    FREE

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,14 +10,14 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *new;
+	listint_t *temp;
 
 	if (head == NULL)
 		return;
 	while (*head != NULL)
 	{
-		new = (*head)->new;
+		temp = (*head)->temp;
 		free(*head);
-		*head = new;
+		*head = temp;
 	}
 }
